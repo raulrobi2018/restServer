c025dd86c66b901361172795778493025a453496
Raul Rodriguez <raulrobi@gmail.com>
2020-12-10 10:37:30 -0300

    Class 80 - Get, Put, Post and Delete requests

--- server/server.js
+++ server/server.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3000;
 
-app.get("/", function (req, res) {
+/* ********************************************* */
+/* These both 'use' sentences are middleweares. 
+Every time a request is send to the server, the middleweares are executed*/
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
+/* ********************************************* */
+
+app.get("/", (req, res) => {
     res.json("Hello World");
+});
+
+app.post("/user", (req, res) => {
+    let body = req.body;
+    res.json({
+        body
+    });
+});
+
+app.put("/user/:id", (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete("/user", (req, res) => {
+    res.json("Delete user");
 });
 
 app.listen(3000, () => {
