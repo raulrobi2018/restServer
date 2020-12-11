f4c555a41ec223634de3935c916ec86840b64b45
Raul Rodriguez <raulrobi@gmail.com>
2020-12-11 07:32:10 -0300

    Class 95 - Custom validation on email and role

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,11 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+// Valid roles for role attribute
+let validRoles = {
+    values: ["ADMIN_ROLE", "USER_ROLE"],
+    message: "{VALUE} is not a valid role"
+};
 
 let Schema = mongoose.Schema;
 
@@ -10,7 +17,8 @@ let userSchema = new Schema({
     },
     email: {
         type: String,
-        required: [true, "The e-mail is required"]
+        required: [true, "The e-mail is required"],
+        unique: true
     },
     password: {
         type: String,
@@ -21,7 +29,9 @@ let userSchema = new Schema({
     },
     role: {
         type: String,
-        default: "USER_ROLE"
+        default: "USER_ROLE",
+        // only valid predifined roles
+        enum: validRoles
     },
     state: {
         type: Boolean,
@@ -32,5 +42,10 @@ let userSchema = new Schema({
         default: false
     }
 });
+
+// Here we are using the unique-validator-mongoose plugin to set the
+// message for the response when we are duplicating a register
+// The PATH word must be between ' or ", not between `
+userSchema.plugin(uniqueValidator, {message: "{PATH} must be unique"});
 
 module.exports = mongoose.model("user", userSchema);
