ea735821872597566b091011f9ff65823171bacd
Raul Rodriguez <raulrobi@gmail.com>
2020-12-10 21:27:41 -0300

    Class 93 - Creating user model

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    name: {
+        type: String,
+        // Sets the name as required and a message
+        required: [true, "The name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "The e-mail is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "The password is required"]
+    },
+    img: {
+        type: String
+    },
+    role: {
+        default: "USER_ROLE"
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model("user", userSchema);
