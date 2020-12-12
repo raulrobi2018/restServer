0412c63bd3352cfc89129c09178159fc965f1c45
Raul Rodriguez <raulrobi@gmail.com>
2020-12-12 07:54:12 -0300

    Class 109 - Connecting mLab with MongoDB Compass

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "dev") {
     urlDatabase = "mongodb://localhost:27017/cofee";
 } else {
     urlDatabase =
-        "mongodb+srv://raulrobi:2AGJglk4RqjhBRPF@cluster0.vrtdh.mongodb.net/<dbname>?retryWrites=true&w=majority";
+        "mongodb+srv://raulrobi:2AGJglk4RqjhBRPF@cluster0.vrtdh.mongodb.net/coffee?retryWrites=true&w=majority";
 }
 
 process.env.URLDB = urlDatabase;
